16600a31736a8703de0c6a77c1b6d8fc9a03093e
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-03-19 10:45:05 +0100

    Fix errors

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,8 +1,8 @@
 class Entity < ApplicationRecord
   belongs_to :user
   has_many :entity_groups, dependent: :destroy
-  has_many :entities, dependent: :destroy
-  has_many :groups
+  # has_many :entities, dependent: :destroy
+  has_many :groups, through: :entity_groups, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :amount, numericality: { greater_than: 0, less_than: 1_000_000 }
